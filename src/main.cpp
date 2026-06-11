8fe681a991838777785c93fb6dc07d525ae6e43a
MikeOxmaul <leva35az@gmail.com>
2026-06-11 14:20:11 +0300

    add argument validation and input file reading

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,33 @@
 #include <QCoreApplication>
+#include <QFile>
+#include <QTextStream>
+#include <QDebug>
+
+#include "errors/errors.h"
+#include "io/file_reader.h"
 
 
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
+    QSet<AppError> errors;
+
+    QStringList args = QCoreApplication::arguments();
+    if (args.size() < 3) {
+        errors.insert(AppError::BadArgCount);
+    } else if (args.size() > 3) {
+        errors.insert(AppError::TooManyArgs);
+    }
+
+    QString text;
+    if(errors.isEmpty())
+        text = readInputFile(args[1], errors);
+
+    if (!errors.isEmpty()) {
+        for (AppError e: errors)
+            qCritical().noquote() << errorMessage(e);
+            return 1;
+    }
+    
     return 0;
 }
