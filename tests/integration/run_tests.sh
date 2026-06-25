1dcb5b75ef170e9144cbc3c744f0b7f84835b84e
MikeOxmaul <leva35az@gmail.com>
2026-06-25 15:39:12 +0300

    update run_tests.sh for coverage

--- tests/integration/run_tests.sh
+++ tests/integration/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PARSER="../../build/KNPO_Java_Parser"
+PARSER="../../build-coverage/KNPO_Java_Parser"
 OUT_DIR="./outputs"
 
 mkdir -p "$OUT_DIR"
@@ -12,5 +12,27 @@ for file in ./inputs/T*.java; do
 
     "$PARSER" "$file" "${OUT_DIR}/${name}"
 done
+
+echo "Test: no args"
+"$PARSER"
+
+echo "Test: only input"
+"$PARSER" ./inputs/T1.java
+
+echo "Test: extra args"
+"$PARSER" ./inputs/T1.java ./outputs/T1 extra
+
+echo "Test: missing file"
+"$PARSER" ./inputs/DOES_NOT_EXIST.java ./outputs/test
+
+echo "Test: empty file"
+touch ./inputs/empty.java
+"$PARSER" ./inputs/empty.java ./outputs/test
+
+echo "Test: no permission file"
+touch ./inputs/noperm.java
+chmod 000 ./inputs/noperm.java
+"$PARSER" ./inputs/noperm.java ./outputs/test
+chmod 644 ./inputs/noperm.java
 
 echo "Done."
